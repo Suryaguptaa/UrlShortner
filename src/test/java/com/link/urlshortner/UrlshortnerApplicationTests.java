4f28cda99a40148ae5446dc3b373d5143a50ee03
Surya Dev Gupta <suryagupta.ws@gmail.com>
2026-02-05 14:19:34 +0530

    intial commit

---
+++ src/test/java/com/link/urlshortner/UrlshortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.link.urlshortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
